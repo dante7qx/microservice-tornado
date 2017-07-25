93a40c046cba0b22c5bafd8391cd11594594d339
dante <sunchao.0129@163.com>
2017-07-25 23:17:14 +0800

    添加统一授权服务

--- tornado-sysmgr-api/src/main/java/com/tornado/TornadoSysMgrAPIApplication.java
+++ tornado-sysmgr-api/src/main/java/com/tornado/TornadoSysMgrAPIApplication.java
@@ -2,10 +2,15 @@ package com.tornado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import com.tornado.common.api.prop.TornadoProperties;
+
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigurationProperties(TornadoProperties.class)
 public class TornadoSysMgrAPIApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TornadoSysMgrAPIApplication.class, args);
