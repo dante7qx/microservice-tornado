f296622dcefe37d54b90fbc8d71987d659c3a937
dante <sunchao.0129@163.com>
2017-07-26 11:44:09 +0800

    添加统一授权服务

--- tornado-common-api/src/main/java/com/feignconfig/FeignClientConfig.java
+++ tornado-common-api/src/main/java/com/feignconfig/FeignClientConfig.java
@@ -1,4 +1,4 @@
-package com.tornado.common.feignconfig;
+package com.feignconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
